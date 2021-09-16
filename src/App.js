997410e72cd438dd61ce218aaecec7efc8932120
fishwdev <fishwdev@gmail.com>
2021-09-16 23:24:53 +0800

    Revert "test"
    This reverts commit cd3ffa76e84cf55e0d710d521175d4a7bbf70db5.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, {Component} from "react";
-import Pokecard from "./Pokecard";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard
-      id={4}
-      name='Charmander'
-      type="fire"
-      base_experience={62}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
